0f7bf3851162ff8ce1e047ccaed94c5047cf6d02
rahul <rahulbeniwal26119@gmail.com>
2019-10-19 21:20:45 +0530

    modified version 1.0

--- car_parking/main.cpp
+++ car_parking/main.cpp
@@ -1,6 +1,22 @@
 #include "park.h"
 int main()
 {
+        int status;
+    string option;
+    printf("Do you want to remove previous save data");
+cin>>option;
+if(option=="yes")
+{
+    status=remove("car.txt");
+    if(status==0)
+    cout<<"successfully deleted, move on ";
+    else
+    cout<<"file delete error , start appending if file exist";
+}
+else
+{
+    cout<<"No Problem, Sir";
+}
     design_box box;
     while(true)
     {
@@ -20,11 +36,15 @@ int main()
         box.box_maker();
         box.box_maker();
         box.box_maker();
-        fstream f("car.txt");
-        f<<"total_car "<<(gate[0]+1)*16<<"\n";
+        cout<<"total_cars"<<(gate[0])*16<<"\n";
+        ofstream f;
         sleep(60);
+        f.open("car.txt",ios::out|ios::app);
+        f<<"total_car "<<(gate[0]+1)*16<<"\n";
         ++gate[0];
 cout<<"total_cars"<<(gate[0])*16<<"\n";
         cout<<"\a\a\a\a\a";
+        system("CLS");
+
     }
 }
